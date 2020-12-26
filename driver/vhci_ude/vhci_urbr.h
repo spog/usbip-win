f4e6d1a0f02e8df93d45ae9a6da5ae9c40088093
Samo Pogačnik <samo_pogacnik@t-2.net>
2020-12-26 12:36:39 +0100

    Added support to display more info about URBs.

--- driver/vhci_ude/vhci_urbr.h
+++ driver/vhci_ude/vhci_urbr.h
@@ -70,3 +70,81 @@ store_urbr(WDFREQUEST req_read, purb_req_t urbr);
 
 extern void
 complete_urbr(purb_req_t urbr, NTSTATUS status);
+
+#if 1 /*spog - added*/
+static const char* get_descriptor = "GET_DESCRIPTOR";
+static const char* get_descriptor_request_device = "GET_DESCRIPTOR Request DEVICE";
+static const char* get_descriptor_request_configuration = "GET_DESCRIPTOR Request CONFIGURATION";
+static const char* get_descriptor_request_string = "GET_DESCRIPTOR Request STRING";
+static const char* set_configuration = "SET_CONFIGURATION";
+static const char* urb_control_in = "URB_CONTROL in";
+static const char* urb_control_out = "URB_CONTROL out";
+static const char* unknown_bRequest = "Unknown 'bRequest' - xxx";
+static const char* unknown_bDescriptorType = "Unknown 'bDescriptorType' - xxx";
+static const char* no_more_info = "-";
+
+static inline const char* urbr_more_info_from_setup_packet(UCHAR* setup_packet)
+{
+	switch (setup_packet[0]) { /*bmRequestType*/
+	case 0x80: /**/
+		switch (setup_packet[1]) { /*bRequest*/
+		case 0x06: /*GET DESCRIPTOR*/
+			switch (setup_packet[3]) { /*bDescriptorType*/
+			case 0x01: return get_descriptor_request_device;
+			case 0x02: return get_descriptor_request_configuration;
+			case 0x03: return get_descriptor_request_string;
+			}
+			return unknown_bDescriptorType;
+		}
+	case 0x00: /**/
+		switch (setup_packet[1]) { /*bRequest*/
+		case 0x09: /*SET CONFIGURATION*/
+			return set_configuration;
+		}
+		return unknown_bRequest;
+	case 0xa1: return urb_control_in;
+	case 0x21: return urb_control_out;
+	}
+	return unknown_bRequest;
+}
+
+static inline const char* urbr_more_info(purb_req_t urbr)
+{
+	PURB urb = urbr->u.urb;
+	USHORT urb_func = urb->UrbHeader.Function;
+
+	switch (urb_func) {
+	case URB_FUNCTION_GET_STATUS_FROM_DEVICE:
+	case URB_FUNCTION_GET_STATUS_FROM_INTERFACE:
+	case URB_FUNCTION_GET_STATUS_FROM_ENDPOINT:
+	case URB_FUNCTION_GET_STATUS_FROM_OTHER:
+		return no_more_info;
+	case URB_FUNCTION_GET_DESCRIPTOR_FROM_DEVICE:
+		return no_more_info;
+	case URB_FUNCTION_GET_DESCRIPTOR_FROM_INTERFACE:
+		return no_more_info;
+	case URB_FUNCTION_CONTROL_TRANSFER:
+		return urbr_more_info_from_setup_packet(((struct _URB_CONTROL_TRANSFER*)urb)->SetupPacket);
+	case URB_FUNCTION_CONTROL_TRANSFER_EX:
+		return urbr_more_info_from_setup_packet(((struct _URB_CONTROL_TRANSFER_EX*)urb)->SetupPacket);
+	case URB_FUNCTION_CLASS_DEVICE:
+	case URB_FUNCTION_CLASS_INTERFACE:
+	case URB_FUNCTION_CLASS_ENDPOINT:
+	case URB_FUNCTION_CLASS_OTHER:
+	case URB_FUNCTION_VENDOR_DEVICE:
+	case URB_FUNCTION_VENDOR_INTERFACE:
+	case URB_FUNCTION_VENDOR_ENDPOINT:
+		return no_more_info;
+	case URB_FUNCTION_BULK_OR_INTERRUPT_TRANSFER:
+		return no_more_info;
+	case URB_FUNCTION_ISOCH_TRANSFER:
+		return no_more_info;
+#if 0
+	case URB_FUNCTION_SYNC_RESET_PIPE_AND_CLEAR_STALL:
+		return no_more_info;
+#endif
+	}
+	return no_more_info;
+
+}
+#endif
